93e4fe0a033b3d9e58cd20d2219397fed0ce1254
adigitallifetechnology <adigitallifetechnology@gmail.com>
2025-11-23 09:35:44 +0000

    Footer: use 3-column grid on md+; remove col-span so columns align

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {/* Company Info */}
-          <div className="col-span-1 md:col-span-2">
+          <div className="col-span-1">
             <div className="flex items-center space-x-3 mb-4">
               <img 
                 src="/logo (6).png" 
