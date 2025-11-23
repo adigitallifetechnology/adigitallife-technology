25555e0521636cb27a80f7c2d09cc684863d46e5
adigitallifetechnology <adigitallifetechnology@gmail.com>
2025-11-23 10:36:05 +0000

    Add ScrollToTop to reset scroll position on route change

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    try {
+      if ('scrollRestoration' in window.history) {
+        window.history.scrollRestoration = 'manual';
+      }
+    } catch (err) {
+      // ignore
+    }
+
+    // scroll to top on route change
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;
